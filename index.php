1d37b07c56203c559071ae3b76451008000560fe
ianmdurrant <49335074+ianmdurrant@users.noreply.github.com>
2019-04-06 09:23:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 <title>Millenium Veterinary Practice</title>
-<link rel="stylesheet" href="reset.css">
+<link rel="stylesheet" href="css/reset.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Martel:400,600|Roboto:300,300i,400,400i,500,500i,700,700i">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
